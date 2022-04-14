3a666235a8de8ca13cb6dc33b0c829c5c50f28e8
Naveenchandar <naveenchandece@gmail.com>
2022-04-14 16:29:28 +0530

    react 18 update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./Context/contextProvider";
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <BrowserRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>,
 );
