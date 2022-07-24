9145de5c7c8ea92560a864cba78bbca09f935047
Naveenchandar <naveenchandece@gmail.com>
2022-07-24 17:29:56 +0530

    service worker register and update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./Context/contextProvider";
-import * as serviceWorker from './service-worker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,4 +18,12 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
